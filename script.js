f06f7d480cc5f1da130ed78e42efbd0153d6ac02
dujimole <dlwltjd345@icloud.com>
2025-04-19 00:25:55 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 
-document.getElementById("start-btn").onclick = function () {
-  document.getElementById("intro-section").classList.add("hidden");
-  document.getElementById("video-section").classList.remove("hidden");
-
-  const video = document.getElementById("intro-video");
-  video.play();
-  video.onended = function () {
-    document.getElementById("video-section").classList.add("hidden");
-    document.getElementById("main-section").classList.remove("hidden");
-  };
-};
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector("#intro").style.display = "flex";
+  document.querySelector("#yesBtn").addEventListener("click", () => {
+    document.querySelector("#intro").style.display = "none";
+    const videoSection = document.querySelector("#videoSection");
+    videoSection.style.display = "block";
+    const video = document.querySelector("#introVideo");
+    video.play();
+    video.onended = () => {
+      videoSection.style.display = "none";
+      document.querySelector("#home").style.display = "block";
+    };
+  });
+});
