68286d426710783fab1a171376bf5746a0553e6e
dujimole <dlwltjd345@icloud.com>
2025-04-18 22:51:08 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
-
-const yesButton = document.getElementById("yes-button");
-const introScreen = document.getElementById("intro-screen");
-const introVideo = document.getElementById("intro-video");
-
-yesButton.addEventListener("click", () => {
-  introScreen.style.display = "none";
-  introVideo.style.display = "block";
-  introVideo.play();
-});
-
-introVideo.addEventListener("ended", () => {
-  window.location.href = "homepage.html";
-});
+function playIntro() {
+    document.querySelector('.hero').style.display = 'none';
+    var video = document.getElementById('introVideo');
+    video.style.display = 'block';
+    video.play();
+    video.onended = function() {
+        window.location.href = "homepage.html";
+    };
+}
